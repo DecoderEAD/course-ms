7bbf4da3e1a982e5aeba8aa649f60523eb62de87
Paulo Viana <pauloc.fviana@hotmail.com>
2022-07-19 23:57:53 -0300

    crud modules

--- src/main/java/com/ead/coursems/services/ModuleService.java
+++ src/main/java/com/ead/coursems/services/ModuleService.java
@@ -2,6 +2,16 @@ package com.ead.coursems.services;
 
 import com.ead.coursems.models.ModuleModel;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ModuleService {
     void delete(ModuleModel moduleModel);
+
+    ModuleModel save(ModuleModel moduleModel);
+
+    Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId);
+
+    List<ModuleModel> findAllByCourse(UUID courseId);
 }
